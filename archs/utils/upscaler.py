b4f1c4e5904101e55df5516b4a903538df61600b
umzi2 <ab5447612@gmail.com>
2023-12-30 16:07:14 +0300

    fix: progress bar

---
+++ archs/utils/upscaler.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+import torch
+from archs import load_model
+from utils.cuda import safe_cuda_cache_empty
+from utils.image import cv_save_image, img2tensor, tensor2img, read_cv
+from utils.tile import auto_split
+from tqdm import tqdm
+from utils.unpickler import RestrictedUnpickle
+
+
+class Upscaler:
+    def __init__(self, model_path, input_folder, output_folder, tile_size=256):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        state_dict = torch.load(
+            model_path, map_location="cpu", pickle_module=RestrictedUnpickle
+        )
+
+        model = load_model(state_dict)
+        model.eval()
+        model = model.to(device)
+
+        self.model = model
+        self.device = device
+        self.input_folder = input_folder
+        self.output_folder = output_folder
+        self.tile_max_size = tile_size
+
+    def __upscale(self, img: np.ndarray) -> np.ndarray:
+        tensor = img2tensor(img).unsqueeze(0).to(self.device)
+
+        with torch.no_grad():
+            tensor = self.model(tensor)
+
+        return tensor2img(tensor)
+
+    def run(self):
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
+
+        for filename in tqdm(os.listdir(self.input_folder), desc=self.model.name, leave=True):
+            input_image_path = os.path.join(self.input_folder, filename)
+            try:
+                img = read_cv(input_image_path)
+                if img is None:
+                    raise RuntimeError(f"Unsupported image type: {filename}")
+
+                result = auto_split(img, self.tile_max_size, self.__upscale)
+
+                output_image_path = os.path.join(self.output_folder, filename)
+                cv_save_image(output_image_path, result, [])
+
+            except RuntimeError as e:
+                print(f"[FAILED] {filename} : {e}")
+            #else:
+            #    print(f"[DONE] {filename}")
+
+        safe_cuda_cache_empty()
