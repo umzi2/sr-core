6349eaa75c3533ed6c0efa42e40825ddac988c4a
umzi <ab5447612@gmail.com>
2024-06-08 22:50:22 +0300

    feat: seemore

--- archs/__init__.py
+++ archs/__init__.py
@@ -12,8 +12,9 @@ from .rgt import RGT
 from .atd import ATD
 from .camixersr import camixersr
 from .plksr import PLKSR
+from .drct import drct
 from .realplksr import realplksr
-
+from .seemore import seemore
 
 def load_model(state_dict) -> PyTorchModel:
     unwrap_keys = ["state_dict", "params_ema", "params-ema", "params", "model", "net"]
@@ -21,6 +22,7 @@ def load_model(state_dict) -> PyTorchModel:
         if key in state_dict and isinstance(state_dict[key], dict):
             state_dict = state_dict[key]
             break
+
 
     state_dict_keys = list(state_dict.keys())
     model: PyTorchModel | None = None
@@ -30,6 +32,8 @@ def load_model(state_dict) -> PyTorchModel:
         model = OmniSR(state_dict)
     elif "body.0.weight" in state_dict_keys and "body.1.weight" in state_dict_keys:
         model = RealESRGANv2(state_dict)
+    elif "body.0.local_block.norm_1.weight" in state_dict_keys:
+        model = seemore(state_dict)
     elif "layers.0.residual_group.blocks.0.norm1.weight" in state_dict_keys:
         model = SwinIR(state_dict)
     elif "layers.0.blocks.2.attn.attn_mask_0" in state_dict_keys:
@@ -40,9 +44,9 @@ def load_model(state_dict) -> PyTorchModel:
     elif "block_1.c1_r.sk.weight" in state_dict_keys:
         model = SPAN(state_dict)
     elif (
-            "conv_final.weight" in state_dict_keys
-            or "unet1.conv1.conv.0.weight" in state_dict_keys
-            or "unet1.conv_bottom.weight" in state_dict_keys
+        "conv_final.weight" in state_dict_keys
+        or "unet1.conv1.conv.0.weight" in state_dict_keys
+        or "unet1.conv_bottom.weight" in state_dict_keys
     ):
         model = cugan(state_dict)
     elif "head.weight" in state_dict_keys:
@@ -50,11 +54,15 @@ def load_model(state_dict) -> PyTorchModel:
     elif 'to_feat.weight' in state_dict_keys:
         model = SAFMN(state_dict)
     elif 'feats.1.channe_mixer.0.weight' in state_dict_keys:
+        print(0)
         model = PLKSR(state_dict)
     elif 'feats.1.channel_mixer.0.weight' in state_dict_keys and "feats.1.norm.weight" in state_dict_keys:
+        print(1)
         model = realplksr(state_dict)
     elif 'relative_position_index_SA' in state_dict_keys:
+        print(2)
         model = ATD(state_dict)
+
 
     else:
         try:
